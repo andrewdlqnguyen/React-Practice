036ac07785b4abf78dda996edcec2bd2bb9baee1
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-08-22 16:31:06 -0400

    passing in index props and using this.props in app container

--- src/containers/App.js
+++ src/containers/App.js
@@ -83,6 +83,7 @@ class App extends Component {
     return (
         <div className={styleApp.App}>
           <Cockpit
+            title={this.props.appTitle}
             showPersons={this.state.showPersons}
             persons={this.state.persons}
             clicked={this.togglePersonsHandler}/>
