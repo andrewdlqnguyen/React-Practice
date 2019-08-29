a59d2ef00b6cdad821b8b4e5e916e6da777dd644
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-08-29 02:03:16 -0400

    Created Auxiliary to replace element wrapper for Person.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
-import stylePerson from './Person.css';
+//import stylePerson from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component {
 
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={[stylePerson.Person, stylePerson.PseudoHover].join(' ')} >
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
+            </Aux>
+            // <div className={[stylePerson.Person, stylePerson.PseudoHover].join(' ')} >
+            //     <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+            //     <p>{this.props.children}</p>
+            //     <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            // </div>
         );
     }
 };
